82b1da07972db49bdd707820a89d6b292e740c15
Sasfi12 <sufyan10.muhammad1070@hotmail.com>
2025-03-04 08:54:14 +0100

    I'm trying stuff

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,13 @@ document.querySelector("body").addEventListener("click", function(e) {
     console.log(e)
 })
 
-
 document.querySelector("#snakeHead").addEventListener("click", function() {
    console.log(this.offsetLeft);
-   this.offsetLeft += 20
-    
+   console.log(this.style.top)
+   if(isNaN(this.style.top)) {
+    this.style.top = this.style.top + 50 + "px" 
+   }
+   else {
+   this.style.top = `${parseInt(this.style.top.split("px").join("")) + 50}px`
+    }
 })
